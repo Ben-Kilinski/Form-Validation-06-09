6ac93eae6d4d85c5fdb1e5ef25b43c7e28a68970
Ben-Kilinski <ben.kilinski@gmail.com>
2023-09-06 16:54:09 +0300

    task 7

--- main.js
+++ main.js
@@ -1,71 +1,96 @@
 //task 1 
-function t1(){
-    if (i1.value == i2.value){
-        alert("welcome")
-        return true
+// function t1(){
+//     if (i1.value == i2.value){
+//         alert("welcome")
+//         return true
         
-    }
-    else{
-        alert("you're not welcome")
-        return false
-    }
-}
+//     }
+//     else{
+//         alert("you're not welcome")
+//         return false
+//     }
+// }
 
 //task 2
-function t2() {
-    if(myInput.value > mySecondInput.value){
-        alert ("first number is bigger")
-        return true
-    }
-    else {
-        alert("you're not welcome")
-        return false
-    }
-}
+// function t2() {
+//     if(myInput.value > mySecondInput.value){
+//         alert ("first number is bigger")
+//         return true
+//     }
+//     else {
+//         alert("you're not welcome")
+//         return false
+//     }
+// }
 
 //task 3
-function t3() {
-    if(input1.value == input2.value){
-        alert ("equal texts")
-        return true 
-    }
-    else {
-        alert("you're not welcome")
-        return false
-    }
-}
+// function t3() {
+//     if(input1.value == input2.value){
+//         alert ("equal texts")
+//         return true 
+//     }
+//     else {
+//         alert("you're not welcome")
+//         return false
+//     }
+// }
 
 //task 4
-function t4 () {
-    if(input4.value.length == input42.value.length){
-        alert ("same size texts")
-        return true 
-    }
-    else {
-        alert("you're not welcome")
-        return false 
-    }
-}
+// function t4 () {
+//     if(input4.value.length == input42.value.length){
+//         alert ("same size texts")
+//         return true 
+//     }
+//     else {
+//         alert("you're not welcome")
+//         return false 
+//     }
+// }
 
 //task 5 
-function t5() {
-    if(input5.value[0] == input52.value[0]){
-        alert("first letters are equal")
-        return true 
-    }
-    else {
-        alert("you're not welcome")
-        return false 
-    }
-}
+// function t5() {
+//     if(input5.value[0] == input52.value[0]){
+//         alert("first letters are equal")
+//         return true 
+//     }
+//     else {
+//         alert("you're not welcome")
+//         return false 
+//     }
+// }
 
 //task 7 
-function t7(){
-    if(username.value.length < 10){
+function valideForm(){
+    var validusername = username.value.length < 10;
+    var validemail = email.value.lastIndexOf(".com") == email.value.length - 4;
+    var validage = new Date().getFullYear() - +age.value.substr(0,4) >= 18;
+    var validcod = cod.value == checkCod.value;
+
+    if (validusername && validemail && validage && validcod){
+        alert("Welcome!")
         return true
     }
     else{
-        username.p
-        return false 
-    }
+        if(!validusername){
+            alert("name bigger than 10 characters")
+            username.style.border = "1px solid red"
+            
+        }
+        if(!validemail){
+            alert("The email dosen't ends with '.com'")
+            email.style.border = "1px solid red"
+            
+        }
+        if(!validage){
+            alert("you're too young")
+            age.style.border = "1px solid red"
+            
+        }
+        if(!validcod){
+            alert("key-numbers are differnts")
+            checkCod.style.border = "1px solid red"
+            
+        }
+    return false
+    } 
 }
