91a5b1f25b4f5a4b176104ff2114b396bd1ae00e
Ben-Kilinski <ben.kilinski@gmail.com>
2023-09-06 14:12:52 +0300

    task 6

--- main.js
+++ main.js
@@ -6,9 +6,8 @@ function t1(){
         
     }
     else{
+        alert("you're not welcome")
         return false
-        
-        
     }
 }
 
@@ -19,6 +18,54 @@ function t2() {
         return true
     }
     else {
+        alert("you're not welcome")
         return false
+    }
+}
+
+//task 3
+function t3() {
+    if(input1.value == input2.value){
+        alert ("equal texts")
+        return true 
+    }
+    else {
+        alert("you're not welcome")
+        return false
+    }
+}
+
+//task 4
+function t4 () {
+    if(input4.value.length == input42.value.length){
+        alert ("same size texts")
+        return true 
+    }
+    else {
+        alert("you're not welcome")
+        return false 
+    }
+}
+
+//task 5 
+function t5() {
+    if(input5.value[0] == input52.value[0]){
+        alert("first letters are equal")
+        return true 
+    }
+    else {
+        alert("you're not welcome")
+        return false 
+    }
+}
+
+//task 7 
+function t7(){
+    if(username.value.length < 10){
+        return true
+    }
+    else{
+        username.p
+        return false 
     }
 }
