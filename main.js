0a54b0577f669fe3ffebbdee85e3dde158b0e43c
Ben-Kilinski <ben.kilinski@gmail.com>
2023-09-06 12:20:59 +0300

    Form Validation task 1

---
+++ main.js
@@ -0,0 +1,13 @@
+//task 1 
+function t1(){
+    if (i1.value == i2.value){
+        alert("welcome")
+        return true
+        
+    }
+    else{
+        return false
+        
+        
+    }
+}
